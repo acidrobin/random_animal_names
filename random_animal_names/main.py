e0be926b73ec02ac14a90244c2ead723b7f9a055
lskafmnasfd <jclayton558@gmail.com>
2022-01-27 20:03:14 +0000

    change

--- random_animal_names/main.py
+++ random_animal_names/main.py
@@ -4,15 +4,15 @@ from importlib import resources
 
 def generate_name():
     
-    with resources.open("animals.txt") as animals_file:
+    with resources.open_text("animals.txt") as animals_file:
         animals = list(animals_file)
         animal = np.random.choice(animals).strip("\n")
 
-    with resources.open("nationalities.txt") as nationalities_file:
+    with resources.open_text("nationalities.txt") as nationalities_file:
         nationalities = list(nationalities_file)
         nationality= np.random.choice(nationalities).strip("\n")
 
-    with resources.open("english-adjectives.txt") as adjectives_file:
+    with resources.open_text("english-adjectives.txt") as adjectives_file:
         adjectives = list(adjectives_file)
         adjective = np.random.choice(adjectives).strip("\n")
 
