5435d29cc0f4a1ff3cf47c7199b3d04ad29077e5
lskafmnasfd <jclayton558@gmail.com>
2022-01-27 19:54:01 +0000

    Made filepaths relative

--- random_animal_names/main.py
+++ random_animal_names/main.py
@@ -2,15 +2,15 @@ import numpy as np
 
 def generate_name():
     
-    with open("animals.txt") as animals_file:
+    with open("./animals.txt") as animals_file:
         animals = list(animals_file)
         animal = np.random.choice(animals).strip("\n")
 
-    with open("nationalities.txt") as nationalities_file:
+    with open("./nationalities.txt") as nationalities_file:
         nationalities = list(nationalities_file)
         nationality= np.random.choice(nationalities).strip("\n")
 
-    with open("english-adjectives.txt") as adjectives_file:
+    with open("./english-adjectives.txt") as adjectives_file:
         adjectives = list(adjectives_file)
         adjective = np.random.choice(adjectives).strip("\n")
 
